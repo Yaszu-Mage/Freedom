3c9c3b66fd2caabf2978a093bf698a0bdc57c527
Yaszu <76853160+Yaszu-Mage@users.noreply.github.com>
2026-03-18 15:24:33 -0700

    Started Revival Logic

--- src/main/java/xyz/yaszu/freedom/Items/Upgrades/Revival.java
+++ src/main/java/xyz/yaszu/freedom/Items/Upgrades/Revival.java
@@ -2,6 +2,7 @@ package xyz.yaszu.freedom.Items.Upgrades;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -9,10 +10,12 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemRarity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.loot.LootTable;
 import org.bukkit.persistence.PersistentDataType;
 import xyz.yaszu.freedom.Items.BaseItem;
+import xyz.yaszu.freedom.Subsystems.Life_and_Death;
 import xyz.yaszu.freedom.Util.Util;
 
 public class Revival extends Util implements BaseItem {
@@ -31,13 +34,33 @@ public class Revival extends Util implements BaseItem {
 
     @Override
     public void effect(Player player, PlayerInteractEvent event) {
-
+        for (Entity entity : player.getNearbyEntities(2,2,2)) {
+            if (entity instanceof Player player_nearby) {
+                if (player_nearby.getPersistentDataContainer().has(keygen("trustedby"))) {
+                    String trustedby = player_nearby.getPersistentDataContainer().get(keygen("trustedby"),PersistentDataType.STRING);
+                    if (trustedby.contains(player.getName())) {
+                        Life_and_Death.revive_player(player_nearby,player.getLocation());
+                        return;
+                    }
+                }
+            }
+        }
     }
 
 
 
     @Override
     public Recipe recipe() {
-        return null;
+        ShapedRecipe rec = new ShapedRecipe(keygen("revivalstone"), item());
+        rec.shape(
+                "NGN",
+                "TST",
+                "NGN"
+        );
+        rec.setIngredient('N', Material.NETHERITE_INGOT);
+        rec.setIngredient('G', Material.GOLDEN_APPLE);
+        rec.setIngredient('T', Material.TOTEM_OF_UNDYING);
+        rec.setIngredient('S', Material.NETHER_STAR);
+        return rec;
     }
 }
