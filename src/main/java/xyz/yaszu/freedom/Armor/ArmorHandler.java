d535311785f5d94d9ec23db38a6fd2ff317f0485
yaszu <sunguildstudios@gmail.com>
2026-06-26 01:26:06 -0700

    Created Java Doc

--- src/main/java/xyz/yaszu/freedom/Armor/ArmorHandler.java
+++
@@ -1,7 +0,0 @@
-package xyz.yaszu.freedom.Armor;
-
-import org.bukkit.event.Listener;
-import xyz.yaszu.freedom.Util.Util;
-
-public class ArmorHandler extends Util implements Listener {
-}
--- src/main/java/xyz/yaszu/freedom/Subsystems/DuelArena.java
+++
@@ -1,7 +0,0 @@
-package xyz.yaszu.freedom.Subsystems;
-
-public enum DuelArena {
-    Small,
-    Medium,
-    Large,
-}
